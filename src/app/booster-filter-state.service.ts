59d02dabfb161d8dd97a54a143397c98b35b33da
Roan <roanlatham@gmail.com>
2024-05-07 11:36:09 +0100

    Addded array functions for enabling or dissabling multiplke boosters

--- src/app/booster-filter-state.service.ts
+++ src/app/booster-filter-state.service.ts
@@ -5,34 +5,39 @@ import { Booster, boosters } from './boosters';
 @Injectable({
   providedIn: 'root',
 })
-export class StratagemFilterStateService {
-  //BehaviorSubject for individual stratagems the user dissabled
+export class BoosterFilterStateService {
+  //BehaviorSubject for individual Boosters the user dissabled
   private disabledIds = new BehaviorSubject<number[]>([]);
   disabledIds$ = this.disabledIds.asObservable();
 
-  disableStratagem(id: number) {
+  disableBooster(id: number) {
     this.disabledIds.next([...this.disabledIds.value, id]);
   }
 
-  enableStratagem(id: number) {
+  enableBooster(id: number) {
     this.disabledIds.next(this.disabledIds.value.filter((i) => i !== id));
   }
 
-  toggleStratagem(id: number) {
-    if (this.disabledIds.value.includes(id)) {
-      this.enableStratagem(id);
-    } else {
-      this.disableStratagem(id);
-    }
-
-    console.log('booster filter service: dissabled IDs: ' + this.disabledIds.value);
+  disableBoosters(ids: number[]) {
+    let newDisabledIds = new Set([...this.disabledIds.value, ...ids]);
+    this.disabledIds.next(Array.from(newDisabledIds));
   }
 
-  toggleWarbond(warbondName: string){
-    
-    let matches: Booster[] = boosters.filter(booster => booster.warbond)
-    // Loop through booster entires, 
-    //save id of boosters that match warbond, 
-    //add to dissabled ids
+  enableBoosters(ids: number[]) {
+    let newDisabledIds = new Set(this.disabledIds.value);
+    ids.forEach(id => newDisabledIds.delete(id));
+    this.disabledIds.next(Array.from(newDisabledIds));
+  }
+
+  toggleBooster(id: number) {
+    if (this.disabledIds.value.includes(id)) {
+      this.enableBooster(id);
+    } else {
+      this.disableBooster(id);
+    }
+
+    console.log(
+      'booster filter service: dissabled IDs: ' + this.disabledIds.value
+    );
   }
 }
