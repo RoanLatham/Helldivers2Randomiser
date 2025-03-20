84e24f3db172877dba1802a571e50272a05576c7
Roan <roanlatham@gmail.com>
2025-03-20 22:12:26 +0000

    refactor(app): simplify AppComponent by removing unused shuffle logic and ids array
    - Removed the shuffle method and the ids array from the AppComponent constructor.
    - Streamlined the constructor for better clarity and maintainability.
    - Ensured the component remains functional by retaining necessary dependencies and methods.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,21 +40,9 @@ import { CollapsibleSectionComponent } from './shared/collapsible-section/collap
 })
 export class AppComponent implements OnInit {
   title = 'HelldiversLoadout';
-  ids: number[] = [];
   filtersCollapsed: boolean = false;
 
-  shuffle(array: number[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
-
-  constructor(private gtagService: GtagService) {
-    const numbers = Array.from({ length: 52 }, (_, i) => i + 1);
-    this.shuffle(numbers);
-    this.ids = numbers.slice(0, 4);
-  }
+  constructor(private gtagService: GtagService) {}
 
   ngOnInit(): void {
     this.gtagService.trackEvent(
@@ -70,6 +58,7 @@ export class AppComponent implements OnInit {
   stratagemRandomiserComponent!: StratagemRandomiserComponent;
   @ViewChild(BoosterRandomiserComponent)
   boosterRandomiserComponent!: BoosterRandomiserComponent;
+
   randomise(): void {
     this.WeaponRandomiserComponent.randomise();
     this.stratagemRandomiserComponent.randomise();
