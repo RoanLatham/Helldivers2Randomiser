4979b27ca008a20859b5a2a2b57acb161cb49cd6
Roan <roanlatham@gmail.com>
2025-03-20 18:24:07 +0000

    feat(stratagems): add new stratagem data and assets
    - Introduced a comprehensive set of stratagem data, including various types categorized by ship module and warbond.
    - Added corresponding image assets for each stratagem, enhancing visual representation in the application.
    - Organized stratagems for improved filtering by ship module and warbond.

---
+++ src/app/new-stratagems.ts
@@ -0,0 +1,676 @@
+export interface Stratagem {
+  id: string;
+  name: string;
+  shipModule: string;
+  iconPath: string;
+  warbond?: string;
+}
+
+export const stratagems: Stratagem[] = [
+  {
+    id: 'MG-43_Machine_Gun',
+
+    name: 'MG-43 Machine Gun',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/MG-43_Machine_Gun.png',
+  },
+  {
+    id: 'APW-1_Anti-Materiel_Rifle',
+
+    name: 'APW-1 Anti-Materiel Rifle',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/APW-1_Anti-Materiel_Rifle.png',
+  },
+  {
+    id: 'M-105_Stalwart',
+
+    name: 'M-105 Stalwart',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/M-105_Stalwart.png',
+  },
+  {
+    id: 'EAT-17_Expendable_Anti-Tank',
+
+    name: 'EAT-17 Expendable Anti-Tank',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/EAT-17_Expendable_Anti-Tank.png',
+  },
+  {
+    id: 'GR-8_Recoilless_Rifle',
+
+    name: 'GR-8 Recoilless Rifle',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/GR-8_Recoilless_Rifle.png',
+  },
+  {
+    id: 'FLAM-40_Flamethrower',
+
+    name: 'FLAM-40 Flamethrower',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/FLAM-40_Flamethrower.png',
+  },
+  {
+    id: 'AC-8_Autocannon',
+
+    name: 'AC-8 Autocannon',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/AC-8_Autocannon.png',
+  },
+  {
+    id: 'MG-206_Heavy_Machine_Gun',
+
+    name: 'MG-206 Heavy Machine Gun',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/MG-206_Heavy_Machine_Gun.png',
+  },
+  {
+    id: 'RL-77_Airburst_Rocket_Launcher',
+
+    name: 'RL-77 Airburst Rocket Launcher',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/RL-77_Airburst_Rocket_Launcher.png',
+  },
+  {
+    id: 'MLS-4X_Commando',
+
+    name: 'MLS-4X Commando',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/MLS-4X_Commando.png',
+  },
+  {
+    id: 'RS-422_Railgun',
+
+    name: 'RS-422 Railgun',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/RS-422_Railgun.png',
+  },
+  {
+    id: 'FAF-14_Spear',
+
+    name: 'FAF-14 Spear',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/FAF-14_Spear.png',
+  },
+  {
+    id: 'StA-X3_WASP_Launcher',
+
+    name: 'StA-X3 W.A.S.P. Launcher',
+
+    shipModule: 'Patriotic Administration Center',
+
+    iconPath:
+      './assets/NewStratagems/Patriotic%20Administration%20Center/StA-X3_WASP_Launcher.png',
+  },
+  {
+    id: 'Orbital_Gatling_Barrage',
+
+    name: 'Orbital Gatling Barrage',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath:
+      './assets/NewStratagems/Orbital%20Cannons/Orbital_Gatling_Barrage.png',
+  },
+  {
+    id: 'Orbital_Airburst_Strike',
+
+    name: 'Orbital Airburst Strike',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath:
+      './assets/NewStratagems/Orbital%20Cannons/Orbital_Airburst_Strike.png',
+  },
+  {
+    id: 'Orbital_120mm_HE_Barrage',
+
+    name: 'Orbital 120mm HE Barrage',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath:
+      './assets/NewStratagems/Orbital%20Cannons/Orbital_120mm_HE_Barrage.png',
+  },
+  {
+    id: 'Orbital_380mm_HE_Barrage',
+
+    name: 'Orbital 380mm HE Barrage',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath:
+      './assets/NewStratagems/Orbital%20Cannons/Orbital_380mm_HE_Barrage.png',
+  },
+  {
+    id: 'Orbital_Walking_Barrage',
+
+    name: 'Orbital Walking Barrage',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath:
+      './assets/NewStratagems/Orbital%20Cannons/Orbital_Walking_Barrage.png',
+  },
+  {
+    id: 'Orbital_Laser',
+
+    name: 'Orbital Laser',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath: './assets/NewStratagems/Orbital%20Cannons/Orbital_Laser.png',
+  },
+  {
+    id: 'Orbital_Napalm_Barrage',
+
+    name: 'Orbital Napalm Barrage',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath:
+      './assets/NewStratagems/Orbital%20Cannons/Orbital_Napalm_Barrage.png',
+  },
+  {
+    id: 'Orbital_Railcannon_Strike',
+
+    name: 'Orbital Railcannon Strike',
+
+    shipModule: 'Orbital Cannons',
+
+    iconPath:
+      './assets/NewStratagems/Orbital%20Cannons/Orbital_Railcannon_Strike.png',
+  },
+  {
+    id: 'Eagle_Strafing_Run',
+
+    name: 'Eagle Strafing Run',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/Eagle_Strafing_Run.png',
+  },
+  {
+    id: 'Eagle_Airstrike',
+
+    name: 'Eagle Airstrike',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/Eagle_Airstrike.png',
+  },
+  {
+    id: 'Eagle_Cluster_Bomb',
+
+    name: 'Eagle Cluster Bomb',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/Eagle_Cluster_Bomb.png',
+  },
+  {
+    id: 'Eagle_Napalm_Airstrike',
+
+    name: 'Eagle Napalm Airstrike',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/Eagle_Napalm_Airstrike.png',
+  },
+  {
+    id: 'LIFT-850_Jump_Pack',
+
+    name: 'LIFT-850 Jump Pack',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/LIFT-850_Jump_Pack.png',
+  },
+  {
+    id: 'Eagle_Smoke_Strike',
+
+    name: 'Eagle Smoke Strike',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/Eagle_Smoke_Strike.png',
+  },
+  {
+    id: 'Eagle_110mm_Rocket_Pods',
+
+    name: 'Eagle 110mm Rocket Pods',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/Eagle_110mm_Rocket_Pods.png',
+  },
+  {
+    id: 'Eagle_500kg_Bomb',
+
+    name: 'Eagle 500kg Bomb',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/Eagle_500kg_Bomb.png',
+  },
+  {
+    id: 'M-102_Fast_Recon_Vehicle',
+
+    name: 'M-102 Fast Recon Vehicle',
+
+    shipModule: 'Hangar',
+
+    iconPath: './assets/NewStratagems/Hangar/M-102_Fast_Recon_Vehicle.png',
+  },
+  {
+    id: 'Orbital_Precision_Strike',
+
+    name: 'Orbital Precision Strike',
+
+    shipModule: 'Bridge',
+
+    iconPath: './assets/NewStratagems/Bridge/Orbital_Precision_Strike.png',
+  },
+  {
+    id: 'Orbital_Gas_Strike',
+
+    name: 'Orbital Gas Strike',
+
+    shipModule: 'Bridge',
+
+    iconPath: './assets/NewStratagems/Bridge/Orbital_Gas_Strike.png',
+  },
+  {
+    id: 'Orbital_EMS_Strike',
+
+    name: 'Orbital EMS Strike',
+
+    shipModule: 'Bridge',
+
+    iconPath: './assets/NewStratagems/Bridge/Orbital_EMS_Strike.png',
+  },
+  {
+    id: 'Orbital_Smoke_Strike',
+
+    name: 'Orbital Smoke Strike',
+
+    shipModule: 'Bridge',
+
+    iconPath: './assets/NewStratagems/Bridge/Orbital_Smoke_Strike.png',
+  },
+  {
+    id: 'EMG-101_HMG_Emplacement',
+
+    name: 'E/MG-101 HMG Emplacement',
+
+    shipModule: 'Bridge',
+
+    iconPath: './assets/NewStratagems/Bridge/EMG-101_HMG_Emplacement.png',
+  },
+  {
+    id: 'FX-12_Shield_Generator_Relay',
+
+    name: 'FX-12 Shield Generator Relay',
+
+    shipModule: 'Bridge',
+
+    iconPath: './assets/NewStratagems/Bridge/FX-12_Shield_Generator_Relay.png',
+  },
+  {
+    id: 'AARC-3_Tesla_Tower',
+
+    name: 'A/ARC-3 Tesla Tower',
+
+    shipModule: 'Bridge',
+
+    iconPath: './assets/NewStratagems/Bridge/AARC-3_Tesla_Tower.png',
+  },
+  {
+    id: 'MD-6_Anti-Personnel_Minefield',
+
+    name: 'MD-6 Anti-Personnel Minefield',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/MD-6_Anti-Personnel_Minefield.png',
+  },
+  {
+    id: 'B-1_Supply_Pack',
+
+    name: 'B-1 Supply Pack',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath: './assets/NewStratagems/Engineering%20Bay/B-1_Supply_Pack.png',
+  },
+  {
+    id: 'GL-21_Grenade_Launcher',
+
+    name: 'GL-21 Grenade Launcher',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/GL-21_Grenade_Launcher.png',
+  },
+  {
+    id: 'LAS-98_Laser_Cannon',
+
+    name: 'LAS-98 Laser Cannon',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/LAS-98_Laser_Cannon.png',
+  },
+  {
+    id: 'MD-I4_Incendiary_Mines',
+
+    name: 'MD-I4 Incendiary Mines',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/MD-I4_Incendiary_Mines.png',
+  },
+  {
+    id: 'AXLAS-5_Guard_Dog_Rover',
+
+    name: 'AX/LAS-5 "Guard Dog" Rover',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/AXLAS-5_Guard_Dog_Rover.png',
+  },
+  {
+    id: 'SH-20_Ballistic_Shield_Backpack',
+
+    name: 'SH-20 Ballistic Shield Backpack',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/SH-20_Ballistic_Shield_Backpack.png',
+  },
+  {
+    id: 'ARC-3_Arc_Thrower',
+
+    name: 'ARC-3 Arc Thrower',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath: './assets/NewStratagems/Engineering%20Bay/ARC-3_Arc_Thrower.png',
+  },
+  {
+    id: 'MD-17_Anti-Tank_Mines',
+
+    name: 'MD-17 Anti-Tank Mines',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/MD-17_Anti-Tank_Mines.png',
+  },
+  {
+    id: 'LAS-99_Quasar_Cannon',
+
+    name: 'LAS-99 Quasar Cannon',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/LAS-99_Quasar_Cannon.png',
+  },
+  {
+    id: 'SH-32_Shield_Generator_Pack',
+
+    name: 'SH-32 Shield Generator Pack',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath:
+      './assets/NewStratagems/Engineering%20Bay/SH-32_Shield_Generator_Pack.png',
+  },
+  {
+    id: 'MD-8_Gas_Mines',
+
+    name: 'MD-8 Gas Mines',
+
+    shipModule: 'Engineering Bay',
+
+    iconPath: './assets/NewStratagems/Engineering%20Bay/MD-8_Gas_Mines.png',
+  },
+  {
+    id: 'AMG-43_Machine_Gun_Sentry',
+
+    name: 'A/MG-43 Machine Gun Sentry',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/AMG-43_Machine_Gun_Sentry.png',
+  },
+  {
+    id: 'AG-16_Gatling_Sentry',
+
+    name: 'A/G-16 Gatling Sentry',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/AG-16_Gatling_Sentry.png',
+  },
+  {
+    id: 'AM-12_Mortar_Sentry',
+
+    name: 'A/M-12 Mortar Sentry',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/AM-12_Mortar_Sentry.png',
+  },
+  {
+    id: 'AXAR-23_Guard_Dog',
+
+    name: 'AX/AR-23 "Guard Dog"',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/AXAR-23_Guard_Dog.png',
+  },
+  {
+    id: 'AAC-8_Autocannon_Sentry',
+
+    name: 'A/AC-8 Autocannon Sentry',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/AAC-8_Autocannon_Sentry.png',
+  },
+  {
+    id: 'AMLS-4X_Rocket_Sentry',
+
+    name: 'A/MLS-4X Rocket Sentry',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/AMLS-4X_Rocket_Sentry.png',
+  },
+  {
+    id: 'AM-23_EMS_Mortar_Sentry',
+
+    name: 'A/M-23 EMS Mortar Sentry',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/AM-23_EMS_Mortar_Sentry.png',
+  },
+  {
+    id: 'EXO-45_Patriot_Exosuit',
+
+    name: 'EXO-45 Patriot Exosuit',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/EXO-45_Patriot_Exosuit.png',
+  },
+  {
+    id: 'EXO-49_Emancipator_Exosuit',
+
+    name: 'EXO-49 Emancipator Exosuit',
+
+    shipModule: 'Robotics Workshop',
+
+    iconPath:
+      './assets/NewStratagems/Robotics%20Workshop/EXO-49_Emancipator_Exosuit.png',
+  },
+  {
+    id: 'TX-41_Sterilizer',
+
+    name: 'TX-41 Sterilizer',
+
+    shipModule: 'Warbonds',
+
+    iconPath: './assets/NewStratagems/Warbonds/TX-41_Sterilizer.png',
+
+    warbond: 'Chemical Agents',
+  },
+  {
+    id: 'AXTX-13_Guard_Dog_Dog_Breath',
+
+    name: 'AX/TX-13 "Guard Dog" Dog Breath',
+
+    shipModule: 'Warbonds',
+
+    iconPath:
+      './assets/NewStratagems/Warbonds/AXTX-13_Guard_Dog_Dog_Breath.png',
+
+    warbond: 'Chemical Agents',
+  },
+  {
+    id: 'SH-51_Directional_Shield',
+
+    name: 'SH-51 Directional Shield',
+
+    shipModule: 'Warbonds',
+
+    iconPath: './assets/NewStratagems/Warbonds/SH-51_Directional_Shield.png',
+
+    warbond: 'Urban Legends',
+  },
+  {
+    id: 'EAT-12_Anti-Tank_Emplacement',
+
+    name: 'E/AT-12 Anti-Tank Emplacement',
+
+    shipModule: 'Warbonds',
+
+    iconPath:
+      './assets/NewStratagems/Warbonds/EAT-12_Anti-Tank_Emplacement.png',
+
+    warbond: 'Urban Legends',
+  },
+  {
+    id: 'AFLAM-40_Flame_Sentry',
+
+    name: 'A/FLAM-40 Flame Sentry',
+
+    shipModule: 'Warbonds',
+
+    iconPath: './assets/NewStratagems/Warbonds/AFLAM-40_Flame_Sentry.png',
+
+    warbond: 'Urban Legends',
+  },
+  {
+    id: 'B-100_Portable_Hellbomb',
+
+    name: 'B-100 Portable Hellbomb',
+
+    shipModule: 'Warbonds',
+
+    iconPath: './assets/NewStratagems/Warbonds/B-100_Portable_Hellbomb.png',
+
+    warbond: 'Servants of Freedom',
+  },
+];
+
+// Lookup collections for filtering
+export const stratagemsByShipModule: { [module: string]: Stratagem[] } = {
+  Warbonds: stratagems.filter(
+    (stratagem) => stratagem.shipModule === 'Warbonds'
+  ),
+  Bridge: stratagems.filter((stratagem) => stratagem.shipModule === 'Bridge'),
+  'Engineering Bay': stratagems.filter(
+    (stratagem) => stratagem.shipModule === 'Engineering Bay'
+  ),
+  Hangar: stratagems.filter((stratagem) => stratagem.shipModule === 'Hangar'),
+  'Robotics Workshop': stratagems.filter(
+    (stratagem) => stratagem.shipModule === 'Robotics Workshop'
+  ),
+  'Patriotic Administration Center': stratagems.filter(
+    (stratagem) => stratagem.shipModule === 'Patriotic Administration Center'
+  ),
+  'Orbital Cannons': stratagems.filter(
+    (stratagem) => stratagem.shipModule === 'Orbital Cannons'
+  ),
+};
+
+export const stratagemsByWarbond: { [warbond: string]: Stratagem[] } = {
+  'Urban Legends': stratagems.filter(
+    (stratagem) => stratagem.warbond === 'Urban Legends'
+  ),
+  'Chemical Agents': stratagems.filter(
+    (stratagem) => stratagem.warbond === 'Chemical Agents'
+  ),
+  'Servants of Freedom': stratagems.filter(
+    (stratagem) => stratagem.warbond === 'Servants of Freedom'
+  ),
+};
+
+// Helper function to get a stratagem by ID
+export function getStratagemById(id: string): Stratagem | undefined {
+  return stratagems.find((s) => s.id === id);
+}
