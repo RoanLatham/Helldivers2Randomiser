dd0c55e5de1460572220dfef9dc8a0b9cd675353
Roan <roanlatham@gmail.com>
2025-04-07 21:12:47 +0100

    refactor(app): streamline imports in app.component.ts by removing unused components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,27 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { StratagemDisplayComponent } from './displays/stratagem-display/stratagem-display.component';
 import { StratagemRandomiserComponent } from './randomisers/stratagem-randomiser/stratagem-randomiser.component';
-import { StratagemFiltersComponent } from './filters/stratagem-filters/stratagem-filters.component';
-import { BoosterDisplayComponent } from './displays/booster-display/booster-display.component';
 import { BoosterRandomiserComponent } from './randomisers/booster-randomiser/booster-randomiser.component';
-import { BoosterFiltersComponent } from './filters/booster-filters/booster-filters.component';
-import { WeaponDisplayComponent } from './displays/weapon-display/weapon-display.component';
 import { WeaponRandomiserComponent } from './randomisers/weapon-randomiser/weapon-randomiser.component';
-import { WeaponFiltersComponent } from './filters/weapon-filters/weapon-filters.component';
-import { WarbondFiltersComponent } from './filters/warbond-filters/warbond-filters.component';
 import { RandomiseButtonComponent } from './randomisers/randomise-button/randomise-button.component';
 import { GtagService } from './services/gtag-service.service';
 import { LearnMoreComponent } from './learn-more/learn-more.component';
-import { CollapsibleSectionComponent } from './shared/collapsible-section/collapsible-section.component';
 import { FooterComponent } from './footer/footer.component';
 import { FilterContainerComponent } from './filters/filter-container/filter-container.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        RouterOutlet,
-        StratagemDisplayComponent,
-        StratagemRandomiserComponent,
-        StratagemFiltersComponent,
-        BoosterDisplayComponent,
-        BoosterRandomiserComponent,
-        BoosterFiltersComponent,
-        WeaponDisplayComponent,
-        WeaponRandomiserComponent,
-        WeaponFiltersComponent,
-        WarbondFiltersComponent,
-        RandomiseButtonComponent,
-        LearnMoreComponent,
-        CollapsibleSectionComponent,
-        FooterComponent,
-        FilterContainerComponent,
-    ],
-    providers: [GtagService],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [
+    StratagemRandomiserComponent,
+    BoosterRandomiserComponent,
+    WeaponRandomiserComponent,
+    RandomiseButtonComponent,
+    LearnMoreComponent,
+    FooterComponent,
+    FilterContainerComponent,
+  ],
+  providers: [GtagService],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'HelldiversLoadout';
